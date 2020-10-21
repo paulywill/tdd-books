c826e896bfa66454ddc5e402ad1bcaf9c929a8dd
Paul Gamble <pgamble@gmail.com>
2020-10-21 01:29:32 -0400

    added initial setup for first functional test

---
+++ test/pages/functional.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render, fireEvent, screen } from '../testUtils'
+import { Home } from '../../pages/index'
+import '@testing-library/jest-dom/extend-expect'
+
+//TDD Books: User Story
+describe('Home page', () => {
+  //Bob logs into TDD Books using Google (or Github).
+
+  //Bob can see either his book shelve he added from earlier or it's empty watching for books to be added.
+
+  //Bob happen to see 3 of his books he added from earlier
+
+  //Bob decides to add the rest of the books he's currently reading or has read this year
+
+  //
+
+
+})
